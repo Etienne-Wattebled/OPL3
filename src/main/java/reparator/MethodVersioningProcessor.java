c3ce671eef150387f4dbdc5c5ed9adac54e4ef70
jvdurieu <jvdurieu@live.be>
2016-01-12 11:23:16 +0100

    createFcontionDAppel

--- src/main/java/reparator/MethodVersioningProcessor.java
+++ src/main/java/reparator/MethodVersioningProcessor.java
@@ -5,10 +5,12 @@ import java.util.List;
 import java.util.Set;
 
 import spoon.processing.AbstractProcessor;
+import spoon.reflect.code.*;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtType;
 import spoon.reflect.factory.Factory;
+import spoon.support.reflect.code.CtBlockImpl;
 
 public class MethodVersioningProcessor extends AbstractProcessor<CtClass> {
 	
@@ -41,6 +43,38 @@ public class MethodVersioningProcessor extends AbstractProcessor<CtClass> {
 				//let's try to find the method now
 			}
 		}
+	}
+
+
+	private void createFonctionDAppel(CtClass ctClass, CtMethod methodeSource, List<CtMethod> methodeDeVersions) {
+
+		CtMethod originalMethod = (CtMethod) ctClass.getMethodsByName(methodeSource.getSimpleName()).get(0);
+
+		// create b1 = block vide de ctmethod
+		//CtBlock originalMethodBlock = originalMethod.getBody();
+		CtBlock nwMethBody = new CtBlockImpl();
+		CtCatch ctCatch = null;
+
+		// pour meth in ctmethods :
+		for (CtMethod methodVersion : methodeDeVersions) {
+
+			// add try catch in b1
+			ctCatch = getFactory().Code().createCtCatch("allCatch", Throwable.class, nwMethBody);
+
+			// add return appels de meth in try
+			List<CtExpression<?>> arguments = methodVersion.getParameters();
+			CtInvocation invocation = getFactory().Code().createInvocation((CtExpression<?>) ctCatch, methodVersion.getReference().getOverridingExecutable(), arguments);
+
+			CtReturn ctReturn = getFactory().Core().createReturn();
+			ctReturn.insertBefore(invocation);
+
+			((CtExpression<?>) ctCatch).setTypeCasts((List) new ArrayList(methodVersion.getThrownTypes()));
+		}
+
+		getFactory().Core().createReturn().insertAfter((CtStatement) ctCatch);
+
+		originalMethod.getBody().replace(nwMethBody);
+
 	}
 
 }
